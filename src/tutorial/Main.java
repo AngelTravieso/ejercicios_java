2d1eee077470c126aabfbfa454f4350bf2599d7e
Angel Travieso <angeltraviesoc@gmail.com>
2022-07-22 16:17:23 -0400

    ejercicio 1

---
+++ src/tutorial/Main.java
@@ -0,0 +1,28 @@
+package tutorial;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author LibrePago-02
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // https://www.discoduroderoer.es/ejercicios-propuestos-y-resueltos-basicos-java/
+
+//        Practica1 practica1 = new Practica1();
+        Practica2 practica2 = new Practica2();
+
+    }
+
+}
+
---
+++ src/tutorial/Practica1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutorial;
+
+/**
+ *
+ * @author LibrePago-02
+ */
+public class Practica1 {
+    
+    public Practica1()
+    {
+            // Ejercicio 1
+        var numero1 = 10;
+        var numero2 = 5;
+
+        var suma = numero1 + numero2;
+        var resta = numero1 - numero2;
+        var multiplicacion = numero1 * numero2;
+        var division = numero1 / numero2;
+        var resto = numero1 % numero2;
+
+        System.out.println("suma = " + suma);
+        System.out.println("resta = " + resta);
+        System.out.println("multiplicacion = " + multiplicacion);
+        System.out.println("division = " + division);
+        System.out.println("resto = " + resto);}
+}
