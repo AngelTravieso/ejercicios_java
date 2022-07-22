7615f3176fe379dacaa7bc027fb992890124d6bb
Angel Travieso <angeltraviesoc@gmail.com>
2022-07-22 16:22:15 -0400

    ejercicio 2

--- src/tutorial/Practica2.java
+++ src/tutorial/Practica2.java
@@ -10,4 +10,17 @@ package tutorial;
  */
 public class Practica2 {
     
+    public Practica2() {
+        int numero1 = 4;
+        int numero2 = 6;
+    
+        if (numero1 > numero2)
+            System.out.println("El numero " + numero1 + " es mayor");
+        else if (numero1 == numero2)
+            System.out.println("Los numeros son iguales");
+        else
+            System.out.println("El numero " + numero2 + " es mayor");
+    }
+        
+    
 }
