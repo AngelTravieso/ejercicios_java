5520216bbe8b1438b425cc8c2b25cb3d74fce105
AngelTravieso <angeltraviesoc@gmail.com>
2022-07-24 20:57:48 -0400

    ejercicio 4

---
+++ src/tutorial/Practica4.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutorial;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Practica4 {
+    
+    public Practica4() {
+        
+        Scanner console = new Scanner(System.in);
+        
+        System.out.println("Ingresa tu nombre: ");
+        
+        var nombre = console.nextLine();
+        
+        System.out.println("Bienvenido " + nombre);
+        
+    }
+    
+}
