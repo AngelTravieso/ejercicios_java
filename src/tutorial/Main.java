498aa194180b5db97712b108b0aaac8cb1e3767d
AngelTravieso <angeltraviesoc@gmail.com>
2022-08-07 23:13:36 -0400

    ejercicio 6

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -16,9 +16,12 @@ public class Main {
     public static void main(String[] args) {
 
         // https://www.discoduroderoer.es/ejercicios-propuestos-y-resueltos-basicos-java/
-//        Practica1 practica1 = new Practica1();
+        // Practica1 practica1 = new Practica1();
         // Practica2 practica2 = new Practica2();
-        Practica3 practica3 = new Practica3();
+        // Practica3 practica3 = new Practica3();
+        // Practica4 practica4 = new Practica4();
+        // Practica5 practica5 = new Practica5();
+        Practica6 practica6 = new Practica6();
 
     }
 
