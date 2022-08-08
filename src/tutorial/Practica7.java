daffc741e48dcf434170b98d2e99679bd196f773
AngelTravieso <angeltraviesoc@gmail.com>
2022-08-07 23:25:29 -0400

    ejercicio 7

---
+++ src/tutorial/Practica7.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutorial;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Practica7 {
+    
+    public Practica7() {
+        
+        Scanner console = new Scanner(System.in);
+        
+        System.out.println("Ingresa un caracter:");
+        
+        int character = Integer.parseInt(console.nextLine());
+
+        char caracter=(char)character;
+ 
+        System.out.println(caracter);        
+        
+    }
+    
+}
